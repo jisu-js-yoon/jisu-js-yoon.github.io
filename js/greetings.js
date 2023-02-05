cf1ec96167fe91a7c99887bba5d5c2d6c1a5c67b
Jisu Yoon <jisu@Jisus-MacBook-Pro.local>
2023-02-05 16:47:05 -0500

    Small fixes

--- js/greetings.js
+++ js/greetings.js
@@ -1,6 +1,8 @@
 const loginForm = document.querySelector(".login-form");
 const loginInput = document.querySelector(".login-form input");
 const greeting = document.querySelector(".greeting");
+const todoForm = document.querySelector(".todo-form");
+const placeholder = document.querySelector(".placeholder");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -11,11 +13,13 @@ function onLoginSubmit(event){
    const username = loginInput.value;
    localStorage.setItem(USERNAME_KEY, username);
    paintGreetings(username);
-
+   todoForm.classList.remove(HIDDEN_CLASSNAME);
+   placeholder.classList.add(HIDDEN_CLASSNAME);
 }
 
 function paintGreetings(username){
    greeting.classList.remove(HIDDEN_CLASSNAME);
+   placeholder.classList.add(HIDDEN_CLASSNAME);
    greeting.innerText = `Hello ${username}`;
 }
 
@@ -23,7 +27,9 @@ const savedUserName = localStorage.getItem(USERNAME_KEY);
 
 if(savedUserName===null){
    loginForm.classList.remove(HIDDEN_CLASSNAME);
+   todoForm.classList.add(HIDDEN_CLASSNAME);
    loginForm.addEventListener("submit", onLoginSubmit);
 }else{
    paintGreetings(savedUserName)
+
 }
